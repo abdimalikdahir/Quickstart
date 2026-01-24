5eabd6f184eb8ea59236dca607abf8556e04a139
abdimalikdahir <abdimalikdahir4567@gmail.com>
2026-01-24 08:38:59 -0800

    oh yeah

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RumbleTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RumbleTest.java
@@ -1,65 +1,31 @@
 package org.firstinspires.ftc.teamcode;
 
-<<<<<<< HEAD
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-=======
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.TouchSensor;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.hardware.DcMotor;
->>>>>>> c10ab50917c49f7d913d086fa5258888dd287ab7
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+@TeleOp(name = "RumbleTest")
+public class RumbleTest extends LinearOpMode {
 
-@TeleOp
-<<<<<<< HEAD
-public class RumbleTest {
+    private TouchSensor touchSensor;
 
     @Override
-    public void init() {
+    public void runOpMode() {
 
+        touchSensor = hardwareMap.get(TouchSensor.class, "touchSensor");
 
-    }
+        waitForStart();
 
-    @Override
-    public void loop() {
-        if() {
-            gamepad1.rumble(100);
+        while (opModeIsActive()) {
 
-
-        }
-=======
-        public class RumbleTest extends LinearOpMode{
-            TouchSensor touchSensor;
-            @Override
-            public void runOpMode() {
-                touchSensor = hardwareMap.get(TouchSensor.class, "touchSensor");
-                waitForStart();
-
-                if(opModeIsActive())
-                {
-
-                }
-                while(opModeIsActive())
-                {
-                    if(!touchSensor.isPressed()) {
-                        gamepad1.rumble(100);
-                        telemetry.addData("IS PRESSED" , !touchSensor.isPressed());
-
-
-                    }
-                    telemetry.update();
-                }
-
->>>>>>> c10ab50917c49f7d913d086fa5258888dd287ab7
+            if (touchSensor.isPressed()) {
+                gamepad1.rumble(100); // 100 ms rumble
+                telemetry.addData("Touch Sensor", "PRESSED");
+            } else {
+                telemetry.addData("Touch Sensor", "NOT PRESSED");
             }
 
-
-
+            telemetry.update();
         }
-<<<<<<< HEAD
-                =======
-
-
-                >>>>>>> c10ab50917c49f7d913d086fa5258888dd287ab7
+    }
+}
