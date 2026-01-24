3dc8147652fc154c1d85ff01191bb53bd91142bf
abdimalikdahir <abdimalikdahir4567@gmail.com>
2026-01-24 08:33:31 -0800

    Create RumbleTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RumbleTest.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp
+public class RumbleTest {
+
+    @Override
+    public void init() {
+
+
+    }
+
+    @Override
+    public void loop() {
+        if() {
+            gamepad1.rumble(100);
+
+
+        }
+    }
+
+
+
+}
