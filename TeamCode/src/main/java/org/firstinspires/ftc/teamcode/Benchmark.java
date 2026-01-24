14b12c314879b4ee79c83752bfa094c97f2cc954
abdimalikdahir <abdimalikdahir4567@gmail.com>
2026-01-24 11:46:22 -0800

    oh yeah

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Benchmark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Benchmark.java
@@ -4,27 +4,38 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.TouchSensor;
+
 @TeleOp
 public class Benchmark extends LinearOpMode {
 
-
-    private DcMotor backLeft, backRight, frontLeft, frontRight, flyWheel;
-    private TouchSensor touchSensor;
+    private DcMotor backLeft, backRight, frontLeft, frontRight;
 
     @Override
     public void runOpMode() {
+
+        waitForStart();
+
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-        flyWheel = hardwareMap.get(DcMotor.class, "flyWheel");
 
-        touchSensor = hardwareMap.get(TouchSensor.class, "touchSensor");
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+
 
         backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
 
         backLeft.setTargetPosition(1000);
         backRight.setTargetPosition(1000);
@@ -36,30 +47,20 @@ public class Benchmark extends LinearOpMode {
         frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        backLeft.setPower(0.3);
-        backRight.setPower(0.3);
-        frontLeft.setPower(0.3);
-        frontRight.setPower(0.3);
+        backLeft.setPower(0.6);
+        backRight.setPower(0.6);
+        frontLeft.setPower(0.6);
+        frontRight.setPower(0.6);
 
-        if (touchSensor.isPressed()) {
-            telemetry.addData("Touch Sensor is Working", touchSensor.isPressed());
+        while (opModeIsActive()) {
+            telemetry.addLine("Benchmark complete!");
+            telemetry.addData("Motor BACK LEFT", backLeft.isBusy());
+            telemetry.addData("Motor BACK RIGHT", backRight.isBusy());
+            telemetry.addData("Motor FRONT LEFT", frontLeft.isBusy());
+            telemetry.addData("Motor FRONT RIGHT", frontRight.isBusy());
+            telemetry.update();
+            idle();
         }
 
-        if (backLeft.isBusy()) {
-            telemetry.addData("Motor BACK LEFT is Working", backLeft.isBusy());
-        }
-
-        if (backRight.isBusy()) {
-            telemetry.addData("Motor BACK RIGHT is Working", backRight.isBusy());
-        }
-
-        if (frontLeft.isBusy()) {
-            telemetry.addData("Motor FRONT LEFT is Working", frontLeft.isBusy());
-        }
-
-        if (frontRight.isBusy()) {
-            telemetry.addData("Motor FRONT RIGHT is Working", frontRight.isBusy());
-        }
-        telemetry.update();
     }
 }
